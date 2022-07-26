67a7e732493107cb4f1ac533f39b5c23f33e32ab
Rafa <rrafasrodrigues@gmail.com>
2022-07-26 11:49:50 -0300

    upload via vscode (1)

---
+++ app.js
@@ -0,0 +1,52 @@
+const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20
+const userStart = [230, 10]
+let currentPosition = userStart
+
+//AddUser()
+//criar bloco
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis]
+    this.bottomRight = [xAxis + blockWidth, yAxis]
+    this.topLeft = [xAxis, xAxis + blockHeight]
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+  }
+}
+const blocks = [
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210)
+]
+addBlock()
+/* ============ FUNCTIONS ========= */
+//desenhar  blocos
+function addBlock() {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = blocks[i].bottomLeft[0] + 'px'
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+    grid.appendChild(block)
+  }
+}
+//add user
+//function addUser() {
+const user = document.createElement('div')
+user.classList.add('user')
+user.style.left = currentPosition[0] + 'px'
+user.style.bottom = currentPosition[1] + 'px'
+grid.appendChild(user)
